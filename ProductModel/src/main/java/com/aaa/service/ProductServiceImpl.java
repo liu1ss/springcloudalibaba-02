29902f07a5a48f523e937b6fb8543b62d0eccc32
五指动乾坤 <2515639608@qq.com>
2020-10-17 16:00:46 +0800

    顶顶顶顶

--- ProductModel/src/main/java/com/aaa/service/ProductServiceImpl.java
+++ ProductModel/src/main/java/com/aaa/service/ProductServiceImpl.java
@@ -11,7 +11,7 @@ public class ProductServiceImpl implements IProductService{
 
     @Override
     public List<Product> findAll() {
-        List<Product> productList=new ArrayList<>();
+        List<Product> productList=new ArrayList<Product>();
         Product p=new Product("testp8472",1,"58",78);
         Product p2=new Product("testp284722",2,"78",87);
         productList.add(p);
